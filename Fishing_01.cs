67dd0d334cc77acaeeb476e7150149c423caa3bb
CHANYOUNKIM <128570803+chanyoung1256@users.noreply.github.com>
2023-07-18 11:03:14 +0900

    Add files via upload

---
+++ Fishing_01.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fishing_01 : MonoBehaviour
+{
+    public Animator Fishing;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            Fishing.SetBool("Fishing", true);
+            Invoke("SwitchScene", 2.5f);
+        }
+        else
+        {
+            Fishing.SetBool("Fishing", false);
+        }
+    }
+    
+    void SwitchScene()
+    {
+        SceneManager.LoadScene("Fishzone");
+    }
+}
